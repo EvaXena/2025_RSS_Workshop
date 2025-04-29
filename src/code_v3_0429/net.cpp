665e00a46c692f0b561b4f881c5d42597a8add1c
EvaXena <l2982124385@gmail.com>
2025-04-29 17:55:59 +0800

    code v3

---
+++ src/code_v3_0429/net.cpp
@@ -0,0 +1,148 @@
+#include "net.hpp"
+
+
+//full connecter   is  wrong
+
+//init full connecter    can be replaced by SXLAL generate matrix?
+void Net::initNet(Mat _layer_neuron_num)
+{
+	layer_neuron_num = _layer_neuron_num;
+	layers.resize(layer_neuron_num.size());
+	// generate each layer
+	static int i = 0;
+	for (auto& matrix : layers)
+	{
+
+		matrix.resize(layer_neuron_num[i][0]);
+		for (auto& row : matrix)
+		{
+			row.resize(layer_neuron_num[i][1]);
+		}
+		i = i + 1;
+	}
+
+	cout << "Generate layers,success! --------initNET Stage1 finished!";
+
+	weights.resize(layers.shape()-1);
+	bias.resize(layers.shape()-1);
+	for (static int j = 0;j < (layer.size() - 1);++j)
+	{
+		weights[j].resize(layer_neuron_num[j+1][0]);
+		bias[j].resize(layer_neuron_num[j+1][0]);
+		for (static int k = 0;k < layer_neuron_num[j];++k)
+		{
+			weights[j][k].resize(layer_neuron_num[j+1][1]);
+			bias[j][k].resize(layer_neuron_num[j+1][1]);
+		}
+	}
+
+	cout << "Generate weights and bias,success! --------initNET Stage2 finished!";
+
+}
+
+
+
+
+//random init  full connnecter weight  using SXLAL
+//M means row,N means col,to correspond with SXLAL
+//example: auto randMAT = initWeight<CF,3,4>(M=3,N=4)
+template<class T,int M,int N>
+Mat Net::initweight_and_bias()
+{
+//	Float_Point_Algebra fpa;
+	T matrix[M][N];
+	fpa.RND_MAT<T,M,N>(MATRIX);
+
+	Mat result;
+	for(int i;i < M;++i)
+	{
+		result.emplace_back(matrix[i],matrix[i]+N);
+	}
+	return result;
+}
+
+
+
+
+//init conv_kernel
+void Net::initConvkernel(Convk& kernel,int kernel_size,int i_ch,int o_ch)
+{
+	kernel.resize(o_ch);
+	for(int i = 0;i < o_ch;i++)
+	{
+		kernel[i].resize(i_ch);
+		for(int j = 0;j < i_ch;j++)
+		{
+			kernel[i][j].resize(kernel_size);
+			for(int k = 0;k < kernel_size;k++)
+			{
+				kernel[i][j][k].resize(kernel_size);
+			}
+
+		}
+	}
+
+//	Float_Point_Algebra fpa;
+	for(int a = 0;a < o_ch;a++)
+	{
+		for(int b = 0;b < i_ch;b++)
+		{
+			fpa.RND_MAT_NT(kernel[a][b]);
+		}
+	}
+
+}
+
+//init conv_bias
+void Net::initConvbias(Vec& bias,int o_ch)
+{
+	bias.resize(o_ch);
+//	Float_Point_Algebra fpa;
+	fpa.RND_VEC_NT(bias);
+
+}
+
+
+//generate a full conv_layer
+void Net::add_conv_layer(int i_ch,int o_ch,int kernel_size,int stride = 1,bool padding = 0)
+{
+	//save this layer's cfg
+	conv_configs.emplace_back(i_ch,o_ch,kernel_size,stride,padding);
+	//create this layer's kernel and save it to kernels
+	Convk kernel;
+	initConvkernel(kernel,kernel_size,i_ch,o_ch);
+	conv_kernels.push_back(kernel);
+	//create this layer's bias and save it to biases
+	Vec bias;
+	initConvbias(bias);
+	conv_biases.push_back(bias);
+}
+
+
+//compute a full conv_layer
+Tri Net::compute_conv_layer(const Tri& input)
+{
+	auto& [i_ch,o_ch,kernel_size,stride,padding] = conv_configs[Conv_stage_number];
+	auto& kernel = conv_kernels[Conv_stage_number];
+	auto& bias = conv_biases[Conv_stage_number];
+	Tri output;
+	fpa.CONV_2D_NT(input,kernel,bias,output,padding,stride);
+	return output;
+	Conv_stage_number = Conv_stage_number + 1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/code_v3_0429/net.hpp
@@ -0,0 +1,93 @@
+#ifndef NET_H
+#define NET_H
+#endif
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "xfxpt_algebra.hpp"
+
+
+#define W 4
+#define I 2
+#define CF ap_fixed<W,I>
+#define CFL ap_fixed<(2*W),(2*I)>
+
+
+#define WEIGHT 256
+#define HIGH 256
+
+using namespace std;
+
+using Vec = vector<CF>;
+using Mat = vector<vector<CF>>;
+using Tri = vector<vector<vector<CF>>>;
+using Convk = vector<vector<vector<vector<CF>>>>;
+using Conv_config = vector<tuple<int,int,int,int,int>>;
+
+
+namespace liu
+{
+	class Net
+	{
+	public:
+
+//		//to create full connecter
+//		Mat layer_neuron_num;  //double
+//		Tri layers;  //triple
+//		Tri weights;  //triple
+//		Tri bias;  //triple
+//		Mat layer;  //double
+
+		//using SXLAL
+		Float_Point_Algebra fpa;
+
+
+		//to create conv layers
+		vector<Tri> conv_layers;
+		vector<Convk> conv_kernels;
+		vector<Vec> conv_biases;
+		Conv_config conv_configs;//configs
+		int Conv_stage_number = 0;//to record the current number of Convstage
+
+
+		//to save feature pictures
+		Tri input_buffer;
+		Tri output_buffer;
+
+
+
+	public:
+		//initialize net to generate params
+		Net(){};
+		~Net(){};
+
+		void initNet(vector<CF> _layer_neuron_num);
+
+		void initWeights();
+
+		void initBias();
+		//////////////////////////
+
+		void initConvkernel(Convk kernel,int kernel_size,int i_ch,int o_ch);
+
+		void add_conv_layer(int i_ch,int o_ch,int kernel_size,int stride = 1,bool padding = 0);
+
+		Tri compute_conv_layer(const Tri& input);
+
+		void forward();
+
+		void backward();
+
+	protected:
+		//init single layer's weight
+		template<class T,int M,int N>
+		Mat initweight_and_bias();
+		//compute delta error
+		void deltaError();
+		//update weights
+		void updateWeights();
+
+
+	};
+
+}
